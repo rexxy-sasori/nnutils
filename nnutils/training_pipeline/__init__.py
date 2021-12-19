e666d08b60bf79f8939fee5b41e44dbb42ff3989
Rex Geng <hgeng4@illinois.edu>
2021-12-19 12:18:37 +0800

    import issue

--- nnutils/training_pipeline/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import accuracy_evaluator
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='nn_pipeline',  # How you named your package folder (MyLib)
     packages=['nnutils'],  # Chose the same as "name"
-    version='0.4',  # Start with a small number and increase it with every change you make
+    version='0.5',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Common utility functions for training and evaluating neural networks as well as analyzing computational complexity',
     # Give a short description about your library
@@ -11,7 +11,7 @@ setup(
     author_email='hgeng4@illinois.edu',  # Type in your E-Mail
     url='https://github.com/rexxy-sasori/nnutils.git',
     # Provide either the link to your github or to your website
-    download_url='https://github.com/rexxy-sasori/nnutils/archive/refs/tags/v0.4.tar.gz',  # I explain this later on
+    download_url='https://github.com/rexxy-sasori/nnutils/archive/refs/tags/v0.5.tar.gz',  # I explain this later on
     keywords=['Complexity', 'Profiler', 'CNN'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'torch>=1.8.1',
