5e749403c35debfb6bfd542b304f4abe543d0ab6
Rex Geng <hgeng4@illinois.edu>
2021-12-19 14:35:08 +0800

    latency_evaluator

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'nnutils.training_pipeline',
         'nnutils.training_pipeline.trainers',
     ],  # Chose the same as "name"
-    version='0.6',  # Start with a small number and increase it with every change you make
+    version='0.7',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Common utility functions for training and evaluating neural networks as well as analyzing computational complexity',
     # Give a short description about your library
@@ -16,7 +16,7 @@ setup(
     author_email='hgeng4@illinois.edu',  # Type in your E-Mail
     url='https://github.com/rexxy-sasori/nnutils.git',
     # Provide either the link to your github or to your website
-    download_url='https://github.com/rexxy-sasori/nnutils/archive/refs/tags/v0.6.tar.gz',  # I explain this later on
+    download_url='https://github.com/rexxy-sasori/nnutils/archive/refs/tags/v0.7.tar.gz',  # I explain this later on
     keywords=['Complexity', 'Profiler', 'CNN'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'torch>=1.8.1',
