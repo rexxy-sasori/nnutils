41cc13e1010fb12137d86d5100361bccd6f66368
Rex Geng <hgeng4@illinois.edu>
2022-01-08 10:41:45 +0800

    removed redefined method + redundant imports

--- nnutils/training_pipeline/latency_evaluator.py
+++ nnutils/training_pipeline/latency_evaluator.py
@@ -12,8 +12,6 @@ import torch
 from nnutils.training_pipeline.trainers.utils import SuppressPrints
 
 
-# from torch_sparse import spmm
-
 def evaluate_latency(model_state, inputs, target_kernel, target_device, num_trials, verbose=False):
     with SuppressPrints(not verbose):
         latency_ms, layerwise_latency_ms = PLATFORM_LATENCY_EVAL[(target_kernel, target_device)](
@@ -70,80 +68,6 @@ def eval_sparsednn(model_state, inputs_dict, target_device, num_trials):
     os.chdir('..')
     return 1000 * total_latency, 1000 * np.array(layerwise_avg_latency)
 
-def eval_sparsednn_c_api(model_state, inputs_dict, target_device, num_trials):
-    import subprocess
-
-    total_latency = 0
-    layerwise_avg_latency = []
-    print('measuring latency for model')
-    time_total = 0
-    start = time.time()
-    os.chdir('sparsednn')
-    for idx, (target_layer_name, input_detail) in enumerate(inputs_dict.items()):
-        weight_tensor = model_state[target_layer_name]
-        if weight_tensor.dim() == 4:
-            cout, cin, hk, wk = weight_tensor.shape
-            M = cout
-            K = cin * hk * wk
-            N = input_detail.output_dim[-1] * input_detail.output_dim[-2]
-            weight = weight_tensor.reshape(M, K).to(target_device).numpy()
-        else:
-            M = weight_tensor.shape[0]
-            K = weight_tensor.shape[1]
-            N = 1
-            weight = weight_tensor.to(target_device).numpy()
-
-        print('start measuring latency using sparsednn, for {}'.format(target_layer_name))
-
-        np.save('matrix_transposed.npy', weight.transpose().astype(np.float32))
-        bias = np.random.normal(size=(M))
-        input = np.random.normal(size=(K,N))
-        np.save('bias.npy',bias.astype(np.float32))
-        np.save('BC.npy',input.astype(np.float32))
-        if N <= 4: continue
-
-        latency = []
-        # code generation
-        ret_code = os.system(f'python code_gen_cpu.py --A_dim {M} --B_dim {K} --C_dim {N} --AT 6 --CT 2 --B_blocks 1 --C_blocks 1 --Gy 1 --infile matrix_transposed.npy --outfile testing.cpp --outfile_asm test1.s --x86 --no_relu --infile_bias bias.npy --fuse')
-        if ret_code != 0:
-            raise Exception(
-                'code generation failed for layer {}, ret_code:{}, M:{}, K:{}, N:{}'.format(target_layer_name,ret_code, M, K, N))
-        
-        # gcc
-        ret_code = os.system("gcc -shared -g test1.s -o test.so")
-        if ret_code != 0 :
-            raise Exception(
-                'gcc failed for layer {}, ret_code:{}, M:{}, K:{}, N:{}'.format(target_layer_name,ret_code, M, K, N))
-        
-        # icc
-        ret_code = os.system(f"icc -fopenmp -I . -qmkl -O3 -march=native -D AT=6 -D CT=2 -D C_Blocks=1 -DA_dim={M} -DINFILE=matrix_transposed.npy -D B_dim={K} -D C_dim={N} -D C_blocks=1 -D X86=1 -D MULTI=0 driver_cpu.cpp -lcnpy -o test -std=c++17")
-        if ret_code != 0 :
-            raise Exception(
-                    'icc compile failed for layer {}, ret_code:{}, M:{}, K:{}, N:{}'.format(target_layer_name,
-                                                                                                    ret_code, M, K, N))
-        for _ in range(num_trials):
-            b = subprocess.run("./test",shell=True,stdout=subprocess.PIPE)
-            if b.returncode != 0:
-                print(b.stdout)
-                raise Exception(
-                    'latency measured failed for layer {}, ret_code:{}, M:{}, K:{}, N:{}'.format(target_layer_name,
-                                                                                                    b.returncode, M, K, N))
- 
-            lines = b.stdout.decode().split('\n')
-            for i,line in enumerate(lines):
-                if 'spmm microkernel' in line:
-                    lat = float(lines[i+1].split()[2])
-                    latency.append(lat)
-
-        total_latency += np.array(latency).mean()
-        layerwise_avg_latency.append(np.array(latency).mean())
-    end = time.time()
-    time_total = end -start
-    print('total measuring cost: {}'.format(time_total))
-    print(total_latency)
-    os.chdir('..')
-    return total_latency,  np.array(layerwise_avg_latency)
-
 
 def eval_sparsednn_c_api(model_state, inputs_dict, target_device, num_trials):
     import subprocess
@@ -178,8 +102,29 @@ def eval_sparsednn_c_api(model_state, inputs_dict, target_device, num_trials):
         if N <= 4: continue
 
         latency = []
+        # code generation
+        ret_code = os.system(
+            f'python code_gen_cpu.py --A_dim {M} --B_dim {K} --C_dim {N} --AT 6 --CT 2 --B_blocks 1 --C_blocks 1 --Gy 1 --infile matrix_transposed.npy --outfile testing.cpp --outfile_asm test1.s --x86 --no_relu --infile_bias bias.npy --fuse')
+        if ret_code != 0:
+            raise Exception(
+                'code generation failed for layer {}, ret_code:{}, M:{}, K:{}, N:{}'.format(target_layer_name, ret_code,
+                                                                                            M, K, N))
+
+        # gcc
+        ret_code = os.system("gcc -shared -g test1.s -o test.so")
+        if ret_code != 0:
+            raise Exception(
+                'gcc failed for layer {}, ret_code:{}, M:{}, K:{}, N:{}'.format(target_layer_name, ret_code, M, K, N))
+
+        # icc
+        ret_code = os.system(
+            f"icc -fopenmp -I . -qmkl -O3 -march=native -D AT=6 -D CT=2 -D C_Blocks=1 -DA_dim={M} -DINFILE=matrix_transposed.npy -D B_dim={K} -D C_dim={N} -D C_blocks=1 -D X86=1 -D MULTI=0 driver_cpu.cpp -lcnpy -o test -std=c++17")
+        if ret_code != 0:
+            raise Exception(
+                'icc compile failed for layer {}, ret_code:{}, M:{}, K:{}, N:{}'.format(target_layer_name,
+                                                                                        ret_code, M, K, N))
         for _ in range(num_trials):
-            b = subprocess.run(f'bash test_sparsednn_c_api.sh {M} {K} {N} 0', shell=True, stdout=subprocess.PIPE)
+            b = subprocess.run("./test", shell=True, stdout=subprocess.PIPE)
             if b.returncode != 0:
                 print(b.stdout)
                 raise Exception(
@@ -199,7 +144,64 @@ def eval_sparsednn_c_api(model_state, inputs_dict, target_device, num_trials):
     print('total measuring cost: {}'.format(time_total))
     print(total_latency)
     os.chdir('..')
-    return 1000 * total_latency, 1000 * np.array(layerwise_avg_latency)
+    return total_latency, np.array(layerwise_avg_latency)
+
+
+# def eval_sparsednn_c_api(model_state, inputs_dict, target_device, num_trials):
+#     import subprocess
+#
+#     total_latency = 0
+#     layerwise_avg_latency = []
+#     print('measuring latency for model')
+#     time_total = 0
+#     start = time.time()
+#     os.chdir('sparsednn')
+#     for idx, (target_layer_name, input_detail) in enumerate(inputs_dict.items()):
+#         weight_tensor = model_state[target_layer_name]
+#         if weight_tensor.dim() == 4:
+#             cout, cin, hk, wk = weight_tensor.shape
+#             M = cout
+#             K = cin * hk * wk
+#             N = input_detail.output_dim[-1] * input_detail.output_dim[-2]
+#             weight = weight_tensor.reshape(M, K).to(target_device).numpy()
+#         else:
+#             M = weight_tensor.shape[0]
+#             K = weight_tensor.shape[1]
+#             N = 1
+#             weight = weight_tensor.to(target_device).numpy()
+#
+#         print('start measuring latency using sparsednn, for {}'.format(target_layer_name))
+#
+#         np.save('matrix_transposed.npy', weight.transpose().astype(np.float32))
+#         bias = np.random.normal(size=(M))
+#         input = np.random.normal(size=(K, N))
+#         np.save('bias.npy', bias.astype(np.float32))
+#         np.save('BC.npy', input.astype(np.float32))
+#         if N <= 4: continue
+#
+#         latency = []
+#         for _ in range(num_trials):
+#             b = subprocess.run(f'bash test_sparsednn_c_api.sh {M} {K} {N} 0', shell=True, stdout=subprocess.PIPE)
+#             if b.returncode != 0:
+#                 print(b.stdout)
+#                 raise Exception(
+#                     'latency measured failed for layer {}, ret_code:{}, M:{}, K:{}, N:{}'.format(target_layer_name,
+#                                                                                                  b.returncode, M, K, N))
+#
+#             lines = b.stdout.decode().split('\n')
+#             for i, line in enumerate(lines):
+#                 if 'spmm microkernel' in line:
+#                     lat = float(lines[i + 1].split()[2])
+#                     latency.append(lat)
+#
+#         total_latency += np.array(latency).mean()
+#         layerwise_avg_latency.append(np.array(latency).mean())
+#     end = time.time()
+#     time_total = end - start
+#     print('total measuring cost: {}'.format(time_total))
+#     print(total_latency)
+#     os.chdir('..')
+#     return 1000 * total_latency, 1000 * np.array(layerwise_avg_latency)
 
 
 def eval_torch_sparse_cuda(model_state, inputs_dict, target_device, num_trials):
@@ -335,7 +337,7 @@ def eval_block_sparse_cuda(model_state, inputs_dict, target_device, num_trials):
 
 PLATFORM_LATENCY_EVAL = {
     ('sparsednn', 'cpu'): eval_sparsednn,
-    ('sparsednn_c_api', 'cpu') : eval_sparsednn_c_api,
+    ('sparsednn_c_api', 'cpu'): eval_sparsednn_c_api,
     ('torch_sparse', 'cuda'): eval_torch_sparse_cuda,
     ('torch_sparse', 'cpu'): eval_torch_sparse_cpu,
 
